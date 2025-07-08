7153ecb80028ed9110ef3a33c75db44c443240ee
Yakshitha <yakshithasenapathi02@gmail.com>
2025-07-08 21:59:28 +0530

    commit

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ app = FastAPI(
 # Allow only frontend dev origins
 origins = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://frontendsch.netlify.app"
 ]
 
 # 1️⃣ Add CORS middleware first
